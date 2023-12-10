d0869e8b69c2305fded13d1dc61b98ae4216b9d5
lesley chang <lesley.hanna98@gmail.com>
2023-12-09 23:09:52 -0800

    resume download link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,39 @@
+import { DownloadIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 import { Layout } from "@/components/layout";
 import { PageHeader, PageSection } from "@/components/page";
 import { SocialLinks } from "@/components/social/social-links";
+import { Button } from "@/components/ui/button";
+import { author } from "@/config/author";
 
 export default function Page() {
   return (
     <Layout>
       <PageSection>
-        <PageHeader title="About" />
-        <div className="mt-8 md:mt-10">
+        <PageHeader
+          title="About"
+          description="Software Engineer - Helping teams build core software from the ground up."
+        />
+        <div className="mt-5 md:mt-8">
           <SocialLinks />
         </div>
+      </PageSection>
+      <PageSection>
+        <div className="space-y-2">
+          <p>⚙️ Previously: Product Engineer @ Ampersand.</p>
+          <p>📚 Studied: Data Science & Statistics @ UC Berkeley.</p>
+        </div>
+        <Button asChild variant="outline" className="mt-8 md:mt-12">
+          <Link
+            href="/resume.pdf"
+            target="_blank"
+            download={`${author.name.replace(" ", "_")}_resume`}
+          >
+            <DownloadIcon className="mr-2" />
+            <span>Download Resume</span>
+          </Link>
+        </Button>
       </PageSection>
     </Layout>
   );
