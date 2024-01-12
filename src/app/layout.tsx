473734cc09db403a9d1b171922f2b5b120480c14
lesley chang <lesley.hanna98@gmail.com>
2024-01-12 11:50:27 -0800

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,40 @@
 import clsx from "clsx";
+import type { Metadata, Viewport } from "next";
 
 import { AnalyticsProvider } from "components/analytics";
 
 import { fonts } from "config/fonts";
-import { rootLayout } from "config/root-layout";
+import { site } from "config/site";
 
 import "styles/main.scss";
 
-export const metadata = rootLayout.metadata;
+export const metadata: Metadata = {
+  metadataBase: new URL(site.url),
+  description: site.description,
+  title: {
+    default: site.title,
+    template: site.title,
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: site.title,
+    description: site.description,
+    url: site.url,
+    siteName: site.title,
+    locale: "en_US",
+    type: "website",
+  },
+};
 
-export const viewport = rootLayout.viewport;
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,
