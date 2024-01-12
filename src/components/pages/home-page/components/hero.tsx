7e8d22625b59f5953c78802fed49c8986beecfc0
lesley chang <lesley.hanna98@gmail.com>
2024-01-12 14:56:47 -0800

    edit hero description

--- src/components/pages/home-page/components/hero.tsx
+++ src/components/pages/home-page/components/hero.tsx
@@ -8,7 +8,7 @@ export const Hero: React.FC = () => {
       <span className="font-mono text-secondary">Hi, my name is</span>
       <PageHeader
         title="Lesley Chang"
-        description="Software Engineer - Helping teams build core software from the ground up."
+        description="Software Engineer. Helping teams build core software from the ground up."
       />
       <div className="mt-8 md:mt-10">
         <SocialLinks />
